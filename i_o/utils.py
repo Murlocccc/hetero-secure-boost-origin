2acb21271775de2392db466aa8bb3f6bc8114d70
cck <1004331338@qq.com>
2021-07-20 21:48:42 +0800

    修改了文件读取的接口

--- i_o/utils.py
+++ i_o/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def read_from_csv(csv_address:str):
+def read_from_csv_with_lable(csv_address: str):
 
     rows = []
 
@@ -25,9 +25,33 @@ def read_from_csv(csv_address:str):
         vals = rows[i].split(',')
 
         lables.append(vals.pop(lable_pos))
-        id = vals.pop(id_pos)
-        ids.append(id)
+        ids.append(vals.pop(id_pos))
         vals = [float(val) for val in vals]
         features.append(np.array(vals))
 
     return header, ids, features, lables
+
+def read_from_csv_with_no_lable(csv_address: str):
+    rows = []
+
+    with open(csv_address) as file:
+        for line in file:
+            rows.append(line.rstrip("\n"))
+    
+    header = rows[0].split(',')
+
+    id_pos = header.index('id')
+    header.pop(id_pos)
+
+    features = []
+    ids = []
+
+    for i in range(1,len(rows)):
+
+        vals = rows[i].split(',')
+
+        ids.append(vals.pop(id_pos))
+        vals = [float(val) for val in vals]
+        features.append(np.array(vals))
+
+    return header, ids, features
