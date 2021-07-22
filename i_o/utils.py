b7ac66cf2348b991cbe4fa2408ecd0491a10a79f
cck <1004331338@qq.com>
2021-07-22 10:14:21 +0800

    增加了序言性注释和文件格式约定

--- i_o/utils.py
+++ i_o/utils.py
@@ -1,6 +1,29 @@
 import numpy as np
 
 def read_from_csv_with_lable(csv_address: str):
+    """
+    从带标签文件读取数据
+
+    Parameters
+    ----------
+    csv_address: str
+        文件路径
+    
+    Returns
+    -------
+    header:
+        list,特征对应的表头
+    ids:
+        list，id的列表
+    features:
+        list，特征的列表，其中特征为numpy.ndarray类型
+    lables:
+        list,标签的列表
+
+    Notes
+    -----
+    csv的第一列会被识别为id列，第二列会被识别为标签列
+    """
 
     rows = []
 
@@ -10,12 +33,13 @@ def read_from_csv_with_lable(csv_address: str):
     
     header = rows[0].split(',')
 
-    lable_pos = header.index('y')
+    # lable_pos = header.index('y')
+    lable_pos = 1
     header.pop(lable_pos)
 
-    id_pos = header.index('id')
+    # id_pos = header.index('id')
+    id_pos = 0
     header.pop(id_pos)
-
     features = []
     lables = []
     ids = []
@@ -32,6 +56,28 @@ def read_from_csv_with_lable(csv_address: str):
     return header, ids, features, lables
 
 def read_from_csv_with_no_lable(csv_address: str):
+    """
+    从不带标签文件读取数据
+
+    Parameters
+    ----------
+    csv_address: str
+        文件路径
+    
+    Returns
+    -------
+    header:
+        list,特征对应的表头
+    ids:
+        list，id的列表
+    features:
+        list，特征的列表，其中特征为numpy.ndarray类型
+
+    Notes
+    -----
+    csv的第一列会被识别为id列
+    """
+
     rows = []
 
     with open(csv_address) as file:
@@ -40,7 +86,8 @@ def read_from_csv_with_no_lable(csv_address: str):
     
     header = rows[0].split(',')
 
-    id_pos = header.index('id')
+    # id_pos = header.index('id')
+    id_pos = 0
     header.pop(id_pos)
 
     features = []
