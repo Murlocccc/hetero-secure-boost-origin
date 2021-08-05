fad1874b9b8253228e02cafc70320df6b3502283
cck <1004331338@qq.com>
2021-08-05 20:53:31 +0800

    用python的logging模块把内部实现给换了

--- ml/utils/logger.py
+++ ml/utils/logger.py
@@ -1,29 +1,48 @@
+import logging
+
+
 
 class LOGGER:
 
-    mask = {
-        'info': True,
-        'debug': True,
-        'debug_data': False,
-        'warning': False,
-    }
+    @classmethod
+    def basic_config(cls, **kwargs):
+        logging.basicConfig(**kwargs)
 
     @classmethod
-    def info(cls, str: str):
-        if cls.mask['info']:
-            print('info: ', str)
+    def info(cls, info: str):
+        logging.info(info)
 
     @classmethod
-    def debug(cls, str: str):
-        if cls.mask['debug']:
-            print('debug: ', str)
+    def debug(cls, debug: str):
+        logging.debug(debug)
 
     @classmethod
-    def debug_data(cls, str: str):
-        if cls.mask['debug_data']:
-            print('debug_data: ', str)
+    def warning(cls, warning: str):
+        logging.warning(warning)
+
+    # mask = {
+    #     'info': True,
+    #     'debug': True,
+    #     'debug_data': False,
+    #     'warning': False,
+    # }
+
+    # @classmethod
+    # def info(cls, str: str):
+    #     if cls.mask['info']:
+    #         print('info: ', str)
+
+    # @classmethod
+    # def debug(cls, str: str):
+    #     if cls.mask['debug']:
+    #         print('debug: ', str)
+
+    # @classmethod
+    # def debug_data(cls, str: str):
+    #     if cls.mask['debug_data']:
+    #         print('debug_data: ', str)
     
-    @classmethod
-    def warning(cls, str: str):
-        if cls.mask['warning']:
-            print('warning: ', str)
+    # @classmethod
+    # def warning(cls, str: str):
+    #     if cls.mask['warning']:
+    #         print('warning: ', str)
